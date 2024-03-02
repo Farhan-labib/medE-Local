9abb61cd5dd347714f8451d8c21469b89d90e76f
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-03-03 01:10:28 +0600

    s

--- medE/settings.py
+++ medE/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/medE/static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/home/mede/medE/static/'
+STATIC_URL = '/medE/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
 
 
