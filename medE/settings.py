39b1a5739dee097d51628f769b5a7081b4521ed7
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-03-03 01:28:34 +0600

    s

--- medE/settings.py
+++ medE/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR,]
+STATICFILES_DIRS=[STATIC_ROOT,]
 
 
 # Default primary key field type
