81da8fbe511a924151cc3eb3f8bcda840097a700
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-03-03 00:56:16 +0600

    static

--- medE/settings.py
+++ medE/settings.py
@@ -18,6 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
+STATIC_ROOT = '/static/'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -152,6 +155,7 @@ USE_TZ = True
 
 STATIC_URL = '/home/mede/medE/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
