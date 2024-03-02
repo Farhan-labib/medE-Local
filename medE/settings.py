dea0df794e5dc123d9b5b4ccedb0e414b3a576ba
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-03-03 01:52:42 +0600

    s

--- medE/settings.py
+++ medE/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -150,8 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR,]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
